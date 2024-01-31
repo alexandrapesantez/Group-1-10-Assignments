deeb8c5048d105b52635ca899c64e11ccb53d941
alexandrapesantez <alexandrapesantez@ALEXANDRAPE3CF7>
2024-01-31 10:03:10 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group_1_10_Assignments.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
